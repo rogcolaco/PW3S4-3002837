dc63ed519cc0e7f4f7e59caee5953db14ab2e826
Rogério Colaço da Silva <colaco.rogerio@gmail.com>
2020-03-09 13:51:46 -0300

    Alterações servelte forum
    Inclusão de persistencia em arquivo e leitura no servlet forum

--- Forum/src/Forum.java
+++ Forum/src/Forum.java
@@ -17,7 +17,7 @@ public class Forum extends HttpServlet{
 		String mensagem = request.getParameter("msg");
 		
 		//GERANDO ARQUIVO
-		FileWriter file = new FileWriter("mensagens.txt", true);
+		FileWriter file = new FileWriter("C:\\Users\\manov\\Downloads\\Forum\\mensagens.txt", true);
 		BufferedWriter buffer = new BufferedWriter(file);
 		buffer.append(email + ";" + mensagem + "\r\n");
 		buffer.close();
@@ -26,9 +26,42 @@ public class Forum extends HttpServlet{
 		//GERANDO RESPOSTA
 		PrintWriter out = response.getWriter();
 		out.println("<html>"
-				+ "<head><title>Calculadora</title></head>"
+				+ "<head><title>Forum</title></head>"
 				+ "<body><h1>Mensagem Salva com Sucesso</h1></body>"
 				+ "</html>");
+		out.close();
+	}
+	
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		String linha = null;
+		ArrayList<String> msgs = new ArrayList<String>();
+				
+		//Lendo arquivo
+		FileReader reader = new FileReader("C:\\Users\\manov\\Downloads\\Forum\\mensagens.txt");
+		BufferedReader leitor = new BufferedReader(reader);
+		while ((linha = leitor.readLine()) != null) {  
+			msgs.add(linha);
+		}  
+		leitor.close();  
+		reader.close();  
+		
+		//Gerando resposta
+		PrintWriter out = response.getWriter();
+		out.println("<html><header><title>Forum</title></header><body>"
+				+ "<h1>Mensagens enviadas</h1>"
+				+ "<hr/>");
+		int i = 1;
+		for ( String text : msgs) {
+			out.println("<h2>Mensagem " + i + "</h2>");
+			i++;
+			String[] msg = text.split(";");
+			//out.println("<p>" + msg + "</p>");
+			out.println("<p>E-mail: " + msg[0] + "</p>"
+					+ "<p>Mensagem: " + msg[1] + "</p>"
+					+ "<hr/>");
+		};
+		out.println("</body></html>");
 		out.close();
 	}
 }
