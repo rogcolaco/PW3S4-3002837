f8cca825aa5697b6589528f4877961044ba521b5
Rogério Colaço da Silva <colaco.rogerio@gmail.com>
2020-03-10 22:22:54 -0300

    Inclusão contador
    - inclui contador forum servlet

--- Forum/src/Forum.java
+++ Forum/src/Forum.java
@@ -21,12 +21,22 @@ public class Forum extends HttpServlet{
 		//Configurando caminho para salvar arquivo
 		filename = getServletContext().getInitParameter("filename");
 		color = getServletConfig().getInitParameter("color");
+		getServletContext().setAttribute("contador", 0);
 	}
 	
 	public void doPost (HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException{
+		
+		//PEGAR INFORMAÇÃO DO CONTEXTO
+		int count = (Integer)getServletContext().getAttribute("contador");
 			
-				
+		//COLOCAR CONTEUDO NO CONTEXTO
+		//primeiro parametro nome (qualquer coisa)
+		//segundo parametro valor do atributo
+		//getServletContext().setAttribute("contador", cont++);
+		
+		
+		
 		//LEITURA DOS PARAMETROS
 		String email = request.getParameter("email");
 		String mensagem = request.getParameter("mensagem");
@@ -35,14 +45,17 @@ public class Forum extends HttpServlet{
 		FileWriter file = new FileWriter(filename, true);
 		BufferedWriter buffer = new BufferedWriter(file);
 		buffer.append(email + ";" + mensagem + "\r\n");
+		getServletContext().setAttribute("contador", ++count);
 		buffer.close();
 		file.close();
+		count = (Integer)getServletContext().getAttribute("contador");
 		
 		//GERANDO RESPOSTA
 		PrintWriter out = response.getWriter();
 		out.println("<html>"
 				+ "<head><title>Forum</title></head>"
-				+ "<body bgcolor="+ color +"><h1>Mensagem Salva com Sucesso</h1></body>"
+				+ "<body bgcolor="+ color +"><h1>Mensagem Salva com Sucesso</h1>"
+				+ "<p>Número de acessos:"+ count + "</p></body>"
 				+ "</html>");
 		out.close();
 	}
